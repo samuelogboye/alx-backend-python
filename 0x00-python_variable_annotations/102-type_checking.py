1953bc8a8328e1456fcf1c32e80d881bcc51d414
Samuel Ogboye <ogboyesam@gmail.com>
2024-01-12 06:00:58 +0100

    more

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Fix Annotation Issue"""
+
+from typing import Tuple, List
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> Tuple:
+    zoomed_in: Tuple = (
+        item for item in lst
+        for i in range(factor)
+    )
+    return tuple(zoomed_in)
+
+
+array = (12, 72, 91)
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
