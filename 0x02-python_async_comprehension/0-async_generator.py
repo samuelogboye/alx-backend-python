b05ffe13df41e6cdd6e63931808901a746e1e14e
Samuel Ogboye <ogboyesam@gmail.com>
2024-01-19 07:39:54 +0100

    Async Generator updated more

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """Async Generator"""
 import asyncio
-from typing import Generator
+from typing import List
 
 
-async def async_generator() -> Generator[float]:
+async def async_generator() -> List[float]:
     """Yield random numbers between 0 and 10"""
     import random
     for i in range(10):
         await asyncio.sleep(1)
-        yield random.random()
+        yield 10 * random.random()
